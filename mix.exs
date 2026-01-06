3d20af2f59f468de941bc833d1bf79216c764b9e
Florian Odronitz <odo@mac.com>
2026-01-06 11:05:55 +0100

    bumping version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AbTest.MixProject do
   def project do
     [
       app: :ab_test,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.18",
       description: description(),
       package: package(),
